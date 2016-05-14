44441bb125c15f0d828db572a8f347509ebbd956
Mark Schaver <schaver@gmail.com>
2016-05-14 16:32:20 -0700

    Routine

--- anonymous.py
+++ anonymous.py
@@ -1,20 +1,20 @@
 import urllib.request
-from xml.etree import ElementTree as ET
+from xml.etree import ElementTree
 
 url = 'https://www.google.com/alerts/feeds/00081205862505704902/15214887296670213833'
 local_file, headers = urllib.request.urlretrieve(url, "C:/Temp/anonymous.txt")
 
-tree = ET.parse(local_file)
+# ElementTree.register_namespace('')
+tree = ElementTree.parse(local_file)
 root = tree.getroot()
 
 entries = root.findall('{http://www.w3.org/2005/Atom}entry')
+
 for entry in entries:
-	print(entry.getchildren())
+    print(entry.getchildren())
 
 # print(list(root))
 
 # Shows all child tags and attributes
 # for child in root:
 # 	print(child.tag)
-
-
