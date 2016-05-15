01e0de454306fef72dbf19d4707eacea11e1b3c9
Mark Schaver <schaver@gmail.com>
2016-05-15 09:07:43 -0700

    Extracts publication title (NYT)

--- anonymous.py
+++ anonymous.py
@@ -1,24 +1,33 @@
 import urllib.request
 from xml.etree import ElementTree
 from sqlite3 import connect
+import re
 
-conn = connect(r'C:\Temp\anon.db')
-curs = conn.cursor()
+#conn = connect(r'C:\Temp\anon.db')
+#curs = conn.cursor()
 
 # TODO: Create loop where will query all anonymous source phrases
-url = 'https://www.google.com/alerts/feeds/00081205862505704902/15214887296670213833'
+# TODO: Alter SQLite table to include name of publication
+# TODO: Extract name of publication to insert in database
+url = 'https://www.google.com/alerts/feeds/00081205862505704902/11030700134857925288'
 local_file, headers = urllib.request.urlretrieve(url, "C:/Temp/anonymous.txt")
 
 tree = ElementTree.parse(local_file)
 root = tree.getroot()
-entries = root.findall('{http://www.w3.org/2005/Atom}entry')
-for entry in entries:
-   entry_title = entry.find('{http://www.w3.org/2005/Atom}title')
-   entry_link = entry.find('{http://www.w3.org/2005/Atom}link')
-   entry_content = entry.find('{http://www.w3.org/2005/Atom}content')
-   insert_values = [entry_title.text, entry_link.attrib['href'], entry_content.text]
-   curs.execute("INSERT INTO anon VALUES (?, ?, ?)", insert_values)
-   conn.commit()
-results = curs.execute("SELECT * FROM anon")
-print(results.fetchall())
+
+title = root.find('{http://www.w3.org/2005/Atom}title')
+pub_name = re.compile('.*new_york_times.*')
+if re.match(pub_name, title.text):
+    source = 'New York Times'
+
+#entries = root.findall('{http://www.w3.org/2005/Atom}entry')
+#for entry in entries:
+#   entry_title = entry.find('{http://www.w3.org/2005/Atom}title')
+#   entry_link = entry.find('{http://www.w3.org/2005/Atom}link')
+#   entry_content = entry.find('{http://www.w3.org/2005/Atom}content')
+#   insert_values = [title.text, entry_title.text, entry_link.attrib['href'], entry_content.text]
+#   curs.execute("INSERT INTO anon VALUES (?, ?, ?, ?)", insert_values)
+#   conn.commit()
+#results = curs.execute("SELECT * FROM anon")
+#print(results.fetchall())
 
