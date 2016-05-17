ab08c28b1b4315591d27176a049401ab4c47f186
Mark Schaver <schaver@gmail.com>
2016-05-16 18:03:14 -0700

    Shortened lines to eliminate PEP 8 errors

--- anonymous.py
+++ anonymous.py
@@ -8,23 +8,25 @@ curs = conn.cursor()
 
 # TODO: Create loop where will query all anonymous source phrases
 # TODO: Remove namespaces from ElementTree find calls
-url = 'https://www.google.com/alerts/feeds/00081205862505704902/11030700134857925288'
+url = 'https://goo.gl/YNsX6Q'
 local_file, headers = urllib.request.urlretrieve(url, "C:/Temp/anonymous.txt")
 
 tree = ElementTree.parse(local_file)
 root = tree.getroot()
-title = root.find('{http://www.w3.org/2005/Atom}title')
+feed_title = root.find('{http://www.w3.org/2005/Atom}title')
 pub_name = re.compile('.*new_york_times.*')
-if re.match(pub_name, title.text):
+if re.match(pub_name, feed_title.text):
     source = 'New York Times'
+source = ''
 entries = root.findall('{http://www.w3.org/2005/Atom}entry')
 for entry in entries:
-   entry_title = entry.find('{http://www.w3.org/2005/Atom}title')
-   entry_link = entry.find('{http://www.w3.org/2005/Atom}link')
-   entry_content = entry.find('{http://www.w3.org/2005/Atom}content')
-   insert_values = [source, entry_title.text, entry_link.attrib['href'], entry_content.text]
-   curs.execute("INSERT INTO anon VALUES (?, ?, ?, ?)", insert_values)
-   conn.commit()
+    title = entry.find('{http://www.w3.org/2005/Atom}title')
+    link = entry.find('{http://www.w3.org/2005/Atom}link')
+    content = entry.find('{http://www.w3.org/2005/Atom}content')
+    insert_values = [source, title.text, link.attrib['href'], content.text]
+    curs.execute("INSERT INTO anon VALUES (?, ?, ?, ?)", insert_values)
+    conn.commit()
 results = curs.execute("SELECT * FROM anon")
-#TODO: Fix encoding error when printing in Visual Studio
-#print(results.fetchall())
+
+# TODO: Fix encoding error when printing in Visual Studio
+# print(results.fetchall())
