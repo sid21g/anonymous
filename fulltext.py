a5afd85f9ef2116feb3734cb16d48ed20ca0d97c
Mark Schaver <schaver@gmail.com>
2016-10-28 21:20:49 -0700

    Daily update

--- fulltext.py
+++ fulltext.py
@@ -10,34 +10,34 @@ def getfulltext():
     anon_curs = anon_conn.cursor()
 
     today = date.today()
-    # str_today = today.strftime('%Y-%m-%d')
-    str_today = "2016-10-26"
+    str_today = today.strftime('%Y-%m-%d')
     anon_curs.execute("SELECT source, link FROM anon WHERE date_entered = ?", (str_today,))
 
     for row in anon_curs.fetchall():
         source = row[0]
         link = row[1]
         a = newspaper.Article(link)
+
         try:
             a.download()
             a.parse()
             print("The article has been parsed.")
-        except Error as e:
-            print("Oops: ", e.args[0])
-        insert_values = [source,
+            insert_values = [source,
                          link,
                          a.text,
                          today]
-        full_conn = connect('C:/Dropbox/Anonymous/fulltext.db')
-        full_curs = full_conn.cursor()
-        try:
-            full_curs.execute("INSERT INTO fulltext VALUES(?, ?, ?, ?)", insert_values)
-            full_conn.commit()
-            print("The full text has been inserted.")
+            full_conn = connect('C:/Dropbox/Anonymous/fulltext.db')
+            full_curs = full_conn.cursor()
+            try:
+                full_curs.execute("INSERT INTO fulltext VALUES(?, ?, ?, ?)", insert_values)
+                full_conn.commit()
+                print("The full text has been inserted.")
+            except Error as e:
+                print("Oops: ", e.args[0])
+            full_conn.close()
         except Error as e:
             print("Oops: ", e.args[0])
-        full_conn.close()
 
     anon_conn.close()
 
-getfulltext()
+# getfulltext()
