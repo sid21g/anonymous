d6bae08ba923086411d9b7eb3b393bfd2f144f4c
Mark Schaver <schaver@gmail.com>
2019-02-05 06:06:05 -0400

    Hackish SQL to eliminate collection pages.

--- dupes.py
+++ dupes.py
@@ -46,7 +46,32 @@ def deletedupes():
     except Error as e:
         print("Oops, duplicate link deletion didn't work: ", e.args[0])
 
-    conn.close()
+    SQL_statements = ["DELETE FROM anon WHERE link LIKE '%http://www.nytimes.com/by/%';",
+                      "DELETE FROM anon WHERE link = 'https://apnews.com/RogerStone';",
+                      "DELETE FROM anon WHERE link = 'https://finance.yahoo.com/news/';",
+                      "DELETE FROM anon WHERE link LIKE '%https://finance.yahoo.com/q/h%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://finance.yahoo.com/quote/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.axios.com/authors/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.cnn.com/videos/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.nytimes.com/search%';",
+                      "DELETE FROM anon WHERE link = 'https://www.politico.com/morningeducation/';",
+                      "DELETE FROM anon WHERE link = 'https://www.politico.com/morningtax/';",
+                      "DELETE FROM anon WHERE link = 'https://www.politico.com/morningtech/';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.politico.com/newsletters/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.politico.com/states/new-york/newsletters/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.reuters.com/entity?%';",
+                      "DELETE FROM anon WHERE link = 'https://www.reuters.com/finance/deals';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.reuters.com/finance/stocks/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.reuters.com/journalists/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.reuters.com/news/archive/%';",
+                      "DELETE FROM anon WHERE link = '%https://www.washingtonpost.com/people/%';",
+                      "DELETE FROM anon WHERE link LIKE '%https://www.wsj.com/livecoverage/%';"]
+
+    for statement in SQL_statements:
+        curs.execute(statement)
+        print("Executed!")
+
+        conn.close()
 
 
 deletedupes()
