c37bdb3146d5cc4e153bce3daf1b27a3e1c9e613
Mark Schaver <schaver@gmail.com>
2019-02-19 19:45:50 -0400

    Added path to server

--- fetch.py
+++ fetch.py
@@ -18,7 +18,7 @@ config.read("/var/www/html/anonymous/config.ini")
 YOUR_ID = config.get("Configuration", "id")
 YOUR_KEY = config.get("Configuration", "key")
 
-conn = connect(r"anon.db")
+conn = connect(r"/var/www/html/anonymous/anon.db")
 curs = conn.cursor()
 today = date.today()
 
