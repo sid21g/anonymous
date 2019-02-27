1570a890d693d6e53451984c0166e944ed21f968
Mark Schaver <schaver@gmail.com>
2019-02-27 11:50:10 -0400

    Added new collection pages.

--- fetch.py
+++ fetch.py
@@ -239,6 +239,7 @@ except Error as e:
 
 SQL_statements = ["DELETE FROM anon WHERE link LIKE '%http://www.nytimes.com/by/%';",
                   "DELETE FROM anon WHERE link LIKE '%https://apnews.com/RogerStone%';",
+                  "DELETE FROM anon WHERE link LIKE '%https://www.axios.com/authors/%';",
                   "DELETE FROM anon WHERE link LIKE '%https://www.cnn.com/videos/%';",
                   "DELETE FROM anon WHERE link LIKE '%https://www.nytimes.com/search%';",
                   "DELETE FROM anon WHERE link LIKE '%https://www.nytimes.com/reuters%';",
@@ -249,8 +250,14 @@ SQL_statements = ["DELETE FROM anon WHERE link LIKE '%http://www.nytimes.com/by/
                   "DELETE FROM anon WHERE link LIKE '%https://www.politico.com/morningtech/%';",
                   "DELETE FROM anon WHERE link LIKE '%https://www.politico.com/newsletters%';",
                   "DELETE FROM anon WHERE link LIKE '%https://www.politico.com/states/new-york/newsletters%';",
+                  "DELETE FROM anon WHERE link LIKE '%https://www.cnbc.com/vladimir-putin%';",
+                  "DELETE FROM anon WHERE link LIKE '%https://www.cnbc.com/mario-draghi/%';",
+                  "DELETE FROM anon WHERE link LIKE '%https://www.cnbc.com/private-equity-and-hedge-funds%';",
+                  "DELETE FROM anon WHERE link LIKE '%https://www.cnbc.com/biotech-and-pharmaceuticals%';",
+                  "DELETE FROM anon WHERE link LIKE '%https://www.cnbc.com/ipos%';",
                   "DELETE FROM anon WHERE link LIKE '%https://www.washingtonpost.com/people/%';",
                   "DELETE FROM anon WHERE link LIKE '%https://www.wsj.com/livecoverage/%';"]
+
 
 for statement in SQL_statements:
     curs.execute(statement)
